bd290ed8d60b481cd314ac9ffe7a6fa141b61db6
Balachandra <51367222+Baluds@users.noreply.github.com>
2026-03-03 17:47:45 +0000

    Data | Resplit the original data

--- sbatch_scripts/sbatch_train_p0.sh
+++ sbatch_scripts/sbatch_train_p0.sh
@@ -3,10 +3,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --time=20:00:00
-#SBATCH --job-name=data-prep
+#SBATCH --job-name=p0-prep
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=80GB
 
 module load conda/latest
 conda activate /work/pi_dagarwal_umass_edu/project_3/bdevarangadi/.conda/envs/diffusion
-python3 /work/pi_dagarwal_umass_edu/project_3/bdevarangadi/Diffusion_as_Memory/testing_refactored.py
+python3 /work/pi_dagarwal_umass_edu/project_3/bdevarangadi/Diffusion_as_Memory/scripts/training/training_dl_augmented.py
