d73b83a73ae56798141d2c7b0c621aa0b17a7a2c
Balachandra <51367222+Baluds@users.noreply.github.com>
2026-04-07 22:42:11 +0000

    sbatch | Added end-to-end script

---
+++ sbatch_scripts/complete-train.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --constraint="gh200|h100|a100|l40s|a40|rtx8000"
+#SBATCH --nodes=1
+#SBATCH --time=30:00:00
+#SBATCH --job-name=denoiser-train
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=80GB
+#SBATCH --output=sbatch_scripts/slurm_output/p1/denoiser-train-%j.out
+#SBATCH --error=sbatch_scripts/slurm_output/p1/denoiser-train-%j.err
+
+nvidia-smi
+
+module load conda/latest
+conda activate /work/pi_dagarwal_umass_edu/project_3/bdevarangadi/.conda/envs/diffusion
+echo "Starting training p0..."
+set -e
+python3 /work/pi_dagarwal_umass_edu/project_3/bdevarangadi/Diffusion_as_Memory/scripts/training/training_dl_augmented.py \
+ --latents-dir ./data/latents/u_dim_new \
+ --checkpoint-dir ./checkpoints/p0/u_dim_new \
+ --output-dir ./output/p0/u_dim_new \
+ --wandb-project diffusion-as-memory \
+ --wandb-run-name p0-training-run_$(date +%Y%m%d_%H%M%S)
+ echo "Training complete!"
+
+################################
+echo "Starting training denoiser..."
+python /work/pi_dagarwal_umass_edu/project_3/bdevarangadi/Diffusion_as_Memory/scripts/training/train_on_latents.py \
+    --train-latents data/latents/u_dim_new/train_latents.pt \
+    --val-latents data/latents/u_dim_new/val_latents.pt \
+    --checkpoint-dir checkpoints/p1/u_dim_new \
+    --wandb-project diffusion-as-memory \
+    --wandb-run-name p1-training-run_$(date +%Y%m%d_%H%M%S)
+echo "Training complete!"
+
+################################
+echo "start training phase 2..."
+python /work/pi_dagarwal_umass_edu/project_3/bdevarangadi/Diffusion_as_Memory/scripts/training/train_phase2.py \
+    --p0-checkpoint ./checkpoints/p0/u_dim_new/best_model.pt \
+    --denoiser-checkpoint ./checkpoints/p1/u_dim_new/best_model.pt \
+    --checkpoint-dir ./checkpoints/p2/u_dim_new_temp \
+    --output-dir ./output/p2/u_dim_new_temp \
+    --data-dir ./data/final \
+    --wandb-project diffusion-as-memory \
+    --wandb-run-name p2-gpsi-run_$(date +%Y%m%d_%H%M%S)
+echo "Training complete!"
