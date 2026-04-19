b5126177651519667c05dd2373259fb8170f0d84
prakritishetty <prakritishetty02@gmail.com>
2026-04-19 15:21:08 +0000

    19thapr

--- sbatch_scripts/sbatch_train_p0.sh
+++ sbatch_scripts/sbatch_train_p0.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-#SBATCH --partition=gpu
+#SBATCH --partition=superpod-a100
 #SBATCH --gres=gpu:1
-#SBATCH --constraint="gh200|h100|a100|l40s|a40|rtx8000"
 #SBATCH --nodes=1
 #SBATCH --time=20:00:00
 #SBATCH --job-name=p0-train
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=80GB
-#SBATCH --output=sbatch_scripts/slurm_output/p0/p0-train-%j.out
-#SBATCH --error=sbatch_scripts/slurm_output/p0/p0-train-%j.err
+#SBATCH --mem-per-cpu=90GB
+#SBATCH --output=slurm_output/p0/p0-train-%j.out
+#SBATCH --error=slurm_output/p0/p0-train-%j.err
+
+echo "Job ID: $SLURM_JOB_ID"
+echo "Node: $SLURM_JOB_NODELIST"
+echo "Start time: $(date)"
+
 
 module load conda/latest
-conda activate /work/pi_dagarwal_umass_edu/project_3/bdevarangadi/.conda/envs/diffusion
-python3 /work/pi_dagarwal_umass_edu/project_3/bdevarangadi/Diffusion_as_Memory/scripts/training/training_dl_augmented.py \
- --latents-dir ./data/latents/rev-diff \
- --checkpoint-dir ./checkpoints/p0/rev-diff \
- --output-dir ./output/p0/rev-diff \
+conda activate ./myvenv
+python3 ../scripts/training/training_dl_augmented.py \
+ --latents-dir ../data/latents/rev-diff \
+ --checkpoint-dir ../checkpoints/p0/train_10thapr_prak/rev-diff \
+ --output-dir ../output/p0/train_10thapr_prak/rev-diff \
  --wandb-project diffusion-as-memory \
  --wandb-run-name p0-training-run_$(date +%Y%m%d_%H%M%S)
+
+echo "End time: $(date)"
